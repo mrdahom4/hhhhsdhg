98454b9100d6546f55d27848734c3a77924294a3
No Nema <44880953+mrdahom4@users.noreply.github.com>
2019-02-14 11:54:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,169 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js"); 
+const bot = new Discord.Client({disableEveryone: true});
+const Canvas = require('canvas') 
+const fs = module.require("fs"); 
+const r1 = require('snekfetch'); 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "simo");
-    logChannel.send(`Invited by: <@${inviter.id}>`);
-  });
+//var يعني تختصر للحاجه زي منا عامل كدة
+
+
+
+
+var mo = "الفلوس"
+var po = "النقاط"
+var lev = "الفل"
+
+
+bot.on("ready", async () => { // كل حاجه هتتفح لما البوت يشتغل
+
+    console.log(`I'm Online \n By ${copy}`) // الي هيظهر في الكونسل
+    console.log(`Logged in as ${bot.user.tag}!`); // نفس الي فوق
+
+    bot.user.setGame(`${bot.users.size} users `,"http://twitch.tv/") 
+    bot.user.setStatus("IdIe")
+
+}); // نهايه الكود
+
+
+
+client.on('message', message => {
+
+if (message.content.startsWith("t!profile")) { // الامر
+ let canvas = new Canvas(300, 300) //حجم الصوره الي هتظهر
+ let ctx = canvas.getContext('2d')
+    let Image = Canvas.Image
+    
+   
+                      //  ava.src = buf;
+
+    fs.readFile(__dirname + '/images_profile/profile.png', function(err, picture) { //مكان الصوره 
+      if (err) throw err
+      var img = new Image
+        		var url = message.author.avatarURL; //افتار صورتك
+		url = url.substring(0, url.indexOf('?'));
+
+		r1.get(url).then(res => {
+			var dataURL = res.body.toString('base64');
+			dataURL = 'data:image/png;base64,' + dataURL;
+			img.onload = function() {
+
+				ctx.save();
+    		ctx.beginPath();
+    		ctx.arc(54, 103, 47, 0, Math.PI * 2, true); // احدثيات الدائره
+		    ctx.closePath();
+		    ctx.clip();
+		    ctx.drawImage(img, 8, 57, 92, 92); // الصوره
+		    ctx.restore();
+			}
+			img.src = dataURL;
+		});
+		
+      img.onload = () => {
+        ctx.drawImage(img, 1, 1, 300, 300)
+     //   ctx.drawImage(message.author.avatarURL, 152, 27, 95, 95);
+        ctx.font = "regular 11px Cairo" // نوع الخط وحجمه
+        ctx.fillStyle = "#9f9f9f" // لون الخط
+        ctx.fillText(`${message.author.username}`, 140, 137)
+        ctx.fillText(`${mo}  `, 143, 219) //money
+        ctx.fillText(`${po}`, 120, 202) // النقاط
+
+        //Level
+        ctx.font = "regular 21px Cairo"
+        ctx.fillStyle = "#ffffff"
+        ctx.fillText(`${lev}`, 47, 255) //لفل
+
+        ctx.save()
+        
+      }
+      img.src = picture
+			
+    })
+		
+   
+
+    
+
+    setTimeout(function() {
+      fs.readFile(__dirname + '/images_profile/diamond_prof_bg.png', function(err, picture) {
+        if (err) throw err
+        var img = new Image
+        img.onload = () => {
+          ctx.drawImage(img, -1, -1, 0, 0)
+        }
+        img.src = picture
+        let inventoryPicture = canvas.toDataURL()
+        let data = inventoryPicture.replace(/^data:image\/\w+;base64,/, "")
+        let buf = new Buffer(data, 'base64')
+      fs.writeFile(`image.png`, buf)
+      
+        message.channel.send("", {
+          file: `image.png` 
+        })
+      })
+    }, 1000)
+
+
+    function roundedImage(x, y, width, height, radius) {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+    }
+
+    function wrapText(context, text, x, y, maxWidth, lineHeight) {
+
+      var words = text.split(' '),
+        line = '',
+        lineCount = 0,
+        i,
+        test,
+        metrics;
+
+      for (i = 0; i < words.length; i++) {
+        test = words[i];
+        metrics = context.measureText(test);
+        while (metrics.width > maxWidth) {
+
+          test = test.substring(0, test.length - 1);
+          metrics = context.measureText(test);
+        }
+        if (words[i] != test) {
+          words.splice(i + 1, 0, words[i].substr(test.length))
+          words[i] = test;
+        }
+
+        test = line + words[i] + ' ';
+        metrics = context.measureText(test);
+
+        if (metrics.width > maxWidth && i > 0) {
+          context.fillText(line, x, y);
+          line = words[i] + ' ';
+          y += lineHeight;
+          lineCount++;
+        } else {
+          line = test;
+        }
+      }
+
+      ctx.fillText(line, x, y);
+    }
+  
+
+
+
+};
+
+
+
+
 });
 
 client.login(process.env.BOT_TOKEN);
